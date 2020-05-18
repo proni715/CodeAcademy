010b9aaf0f0ceff04ec9101b0fcab74e37d9c8dd
proni715 <proni715@gmail.com>
2020-05-18 20:03:12 +0300

    add CardView

--- app/src/main/java/com/example/codeacademy/activities/CourseActivity.java
+++ app/src/main/java/com/example/codeacademy/activities/CourseActivity.java
@@ -103,7 +103,7 @@ public class CourseActivity extends AppCompatActivity {
                 ////
                 ////
                 ////
-                ////
+                ////`
                 linearLayout.addView(card);
 
             }
